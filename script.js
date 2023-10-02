ff92a8e64ad66b8aa66c2dde08b553d0bdc81f8e
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-10-02 12:30:52 +0530

    Solved the errors of Multiple operators
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
 
 let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button =>{
-    button.addEventListener("click", (e)=>{
-        if(e.target.innerHTML == '='){
-             string = eval(string); 
-           // console.log(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML== "DEL"){
-            string = string.substring(0, string.length-1);
-            input.value= string;
-        }
-        else{
-            //console.log(Number(e.target.innerHTML));
-            if (e.target.innerHTML != 0) 
-            string += e.target.innerHTML;
-            else {
-              if (Number(string) > 0) {
-                string += e.target.innerHTML;
-              }
-            }
-            input.value = string;
-        }
-       
-    })
-})
+
+let operators = document.getElementsByClassName("oper");
+
+for (i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", (e) => {
+    if (Number(string) != 0) {
+      let char = string.at(-1);
+      console.log(char);
+      let add = true;
+      if (
+        char == "/" ||
+        char == "%" ||
+        char == "*" ||
+        char == "+" ||
+        char == "-"
+      ) {
+        add = false;
+      }
+
+      if (add == true) string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+}
+
+let numbers = document.getElementsByClassName("numbers");
+for (i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (e) => {
+    if (e.target.innerHTML != 0) string += e.target.innerHTML;
+    else {
+      if (Number(string) != 0) {
+        string += e.target.innerHTML;
+      }
+    }
+    input.value = string;
+  });
+}
+
+document.getElementById("equalButn").addEventListener("click", (e) => {
+  string = eval(string);
+  input.value = string;
+});
+
+document.getElementById("clear").addEventListener("click", (e) => {
+  string = "";
+  input.value = "";
+});
+
+document.getElementById("del").addEventListener("click", (e) => {
+  string = string.slice(0, string.length - 1);
+  input.value = string;
+});
