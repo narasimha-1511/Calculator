ba0e6e65ee1832732ac87605cf5f51b957d002a6
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-10-26 10:42:23 +0530

    Impemented New Currency Calculator Function ..
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 const input = document.getElementById("inputBox");
 let string = "";
 
-const operators = document.getElementsByClassName("oper");
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener("click", (e) => {
-    if (Number(string) !== 0) {
-      const char = string.at(-1);
-      let add = true;
-      if (
-        char === "/" ||
-        char === "%" ||
-        char === "*" ||
-        char === "+" ||
-        char === "-"
-      ) {
-        add = false;
-      }
-      if (add) {
-        string += e.target.innerHTML;
-        input.value = string;
-      }
-    }
-  });
-}
-
-const numbers = document.getElementsByClassName("numbers");
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", (e) => {
-    if (e.target.innerHTML !== "0") {
+const operators = document.querySelectorAll(".oper");
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    const lastChar = string.charAt(string.length - 1);
+    if (
+      lastChar !== "/" &&
+      lastChar !== "%" &&
+      lastChar !== "*" &&
+      lastChar !== "+" &&
+      lastChar !== "-"
+    ) {
       string += e.target.innerHTML;
-    } else {
-      if (Number(string) !== 0) {
-        string += e.target.innerHTML;
-      }
+      input.value = string;
     }
-    input.value = string;
   });
-}
+});
+
+const numbers = document.querySelectorAll(".numbers");
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerHTML !== "0" || string !== "") {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
 
 document.getElementById("equalButn").addEventListener("click", () => {
   string = eval(string);
@@ -49,6 +39,65 @@ document.getElementById("clear").addEventListener("click", () => {
 });
 
 document.getElementById("del").addEventListener("click", () => {
-  string = string.slice(0, string.length - 1);
+  string = string.slice(0, -1);
   input.value = string;
+});
+
+// Toggle between the calculator and currency converter
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+function showTab(tabIndex) {
+  tabContents.forEach((tabContent) => {
+    tabContent.classList.remove("active");
+  });
+  tabContents[tabIndex].classList.add("active");
+}
+
+tabButtons.forEach((tabButton, index) => {
+  tabButton.addEventListener("click", () => {
+    showTab(index);
+  });
+});
+
+// Fetch currency data and populate select options
+(function () {
+  fetch(
+    "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_R4wt6y6yWPY1IiDH8xVieHyykdIf6prVghSqJFTy"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const currencyData = Object.entries(data.data);
+      const targetCurrencySelect = document.getElementById("targetCurrency");
+      const baseCurrencySelect = document.getElementById("baseCurrency");
+      currencyData.forEach((currency) => {
+        const newOption = new Option(currency[0], currency[1]);
+        targetCurrencySelect.add(newOption);
+        baseCurrencySelect.add(newOption.cloneNode(true));
+      });
+    })
+    .catch((error) => console.error(error));
+})();
+
+showTab(0); // Show the Calculator tab by default
+
+function convertCurrency() {
+  const inputAmount = document.getElementById("currencyAmount").value;
+  const baseCurrency = document.getElementById("baseCurrency").value;
+  const targetCurrency = document.getElementById("targetCurrency").value;
+  const result = document.getElementById("result");
+  const currencyAmount = (targetCurrency / baseCurrency) * inputAmount;
+  result.innerHTML = `Converted Amount: ${currencyAmount}`;
+}
+
+document
+  .getElementById("convertCurrency")
+  .addEventListener("click", convertCurrency);
+
+document.getElementById("clearCurrency").addEventListener("click", () => {
+  document.getElementById("currencyAmount").value = "";
+  document.getElementById("baseCurrency").value = "USD";
+  document.getElementById("targetCurrency").value = "USD";
+  document.getElementById("result").innerHTML = "";
+  input.value = "";
 });
